821a4ab5c76e8ac05f8a5707e294ef91f5db97a1
Swantaan <csjakobus@gmail.com>
2025-04-27 14:01:59 +0200

    custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/wound-care-george-site">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/wound-care-george-site/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,

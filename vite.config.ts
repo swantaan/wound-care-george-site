cf88134e0159b6147cf45032982b643761e5370c
Swantaan <csjakobus@gmail.com>
2025-04-27 12:05:56 +0200

    added config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/wound-care-george-site/',
   server: {
     host: "::",
     port: 8080,

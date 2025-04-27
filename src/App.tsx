929b9cf7126dda1d7f0d6a44ee43502820f2492d
Swantaan <csjakobus@gmail.com>
2025-04-27 12:11:49 +0200

    added not found

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/wound-care-george-site">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
